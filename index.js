d867f91b47d945294b2eced29a2d006db04126bb
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-01-22 20:22:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function join() {
-    navigator.clipboard.writeText("LegendOres.minefort.com")
+function joinMessage() {
+    navigator.clipboard.writeText(options["Server IP"])
         .then(() => {
             alert('Copied the IP of the server to your clipboard you can now paste it using: CTRL + V.');
         })
+}
+
+function downloadPack() {
+    window.open(options["Texture Pack Link"], "_blank");
+}
+
+function scrollToSection(sectionId) {
+    var targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    }
 }
---
+++ options.js
@@ -0,0 +1,7 @@
+const options = {
+    // This is the server IP.
+    "Server IP": "LegendOres.minefort.com",
+
+    // This is the link to the texture pack.
+    "Texture Pack Link": "https://download.mc-packs.net/pack/108920415326af0dabc5a459ba46043a2710dfda.zip"
+}
